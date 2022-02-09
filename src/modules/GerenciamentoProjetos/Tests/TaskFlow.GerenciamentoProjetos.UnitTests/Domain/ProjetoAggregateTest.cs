4a1c3c90a860bfa12d3c13628e988aac6624cbc1
doli82 <39415174+doli82@users.noreply.github.com>
2022-02-08 22:13:20 -0300

    Implementados testes do agregado projeto

--- src/modules/GerenciamentoProjetos/Tests/TaskFlow.GerenciamentoProjetos.UnitTests/Domain/ProjetoAggregateTest.cs
+++ src/modules/GerenciamentoProjetos/Tests/TaskFlow.GerenciamentoProjetos.UnitTests/Domain/ProjetoAggregateTest.cs
@@ -1,12 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using TaskFlow.GerenciamentoProjetos.Domain.Exceptions;
+using TaskFlow.GerenciamentoProjetos.Domain.Projeto;
+using Xunit;
 
-namespace TaskFlow.FluxosTrabalho.UnitTests.Domain
+namespace TaskFlow.GerenciamentoProjetos.UnitTests.Domain
 {
-	internal class ProjetoAggregateTest
+	public class ProjetoAggregateTest
 	{
-	}
+
+        [Fact]
+        public void Deve_criar_um_projeto()
+        {
+            //Arrange
+            var nomeProjeto = "Projeto de teste";
+            var descricao = "Descrição do Projeto de teste";
+
+            //Act 
+            var projeto = new Projeto(nomeProjeto, descricao);
+
+            //Assert
+            Assert.NotNull(projeto);
+        }
+
+
+        [Fact]
+        public void Deve_conter_titulo_em_um_novo_projeto()
+        {
+            //Arrange
+            var nomeProjeto = "Projeto de teste";
+            var descricao = "Descrição do Projeto de teste";
+            var projeto1 = new Projeto(nomeProjeto, descricao);
+
+            //Act - Assert
+            Assert.Equal(projeto1.Titulo, nomeProjeto);
+            Assert.Throws<ProjetoDomainException>(() => new Projeto(null, descricao));
+            Assert.Throws<ProjetoDomainException>(() => new Projeto("", descricao));
+        }
+
+        [Fact]
+        public void Deve_conter_descricao_em_um_novo_projeto()
+        {
+            //Arrange
+            var nomeProjeto = "Projeto de teste";
+            var descricao = "Descrição do projeto de teste";
+            var projeto1 = new Projeto(nomeProjeto, descricao);
+
+            //Act - Assert
+            Assert.Equal(projeto1.Titulo, nomeProjeto);
+            Assert.Throws<ProjetoDomainException>(() => new Projeto("Projeto 01", null));
+            Assert.Throws<ProjetoDomainException>(() => new Projeto("Projeto 02",""));
+        }
+    }
 }
